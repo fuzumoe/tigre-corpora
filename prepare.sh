635d0e19f1e4993d381db5df0789caeac6c901df
Adam Fuzum Tewelde <atew@camelot-itlab.com>
2023-12-12 05:54:01 +0100

    first commit

---
+++ prepare.sh
@@ -0,0 +1,6 @@
+sudo apt update -y
+sudo apt install -y tesseract-ocr
+sudo apt install -y libtesseract-dev
+sudo apt install -y poppler-utils
+sudo get install -y tesseract-ocr-amh
+sudo get install -y tesseract-ocr-eng
